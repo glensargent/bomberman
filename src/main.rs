c53b290004948d031a5a3bffd0d335292c658e1c
Glen Sargent <glensarg@hotmail.co.uk>
2021-09-27 20:16:28 +0100

    spawn player

--- src/main.rs
+++ src/main.rs
@@ -33,23 +33,37 @@ impl PlayerBundle {
     }
 }
 
+struct Materials {
+    player_material: Handle<ColorMaterial>,
+}
+
 fn main() {
     App::build()
     .add_plugins(DefaultPlugins)
     // startup system runs once on startup
     .add_startup_system(setup.system())
+    .add_startup_stage("game_setup", SystemStage::single(spawn_player.system()))
     // regular systems run every frame
     .add_system(greet_player.system())
     .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials{
+        player_material: materials.add(Color::rgb(1., 1., 1.).into())
+    });
 }
 
-fn spawn_player(mut commands: Commands) {
+fn spawn_player(mut commands: Commands, materials: Res<Materials>) {
     // create the player entity
-    commands.spawn_bundle(PlayerBundle::new_player("Glen".to_string()));
+    // commands.spawn_bundle(PlayerBundle::new_player("Glen".to_string()));
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.player_material.clone(),
+        sprite: Sprite::new(Vec2::new(10., 10.)),
+        ..Default::default()
+    })
+    .insert_bundle(PlayerBundle::new_player("Glen".to_string()));
 }
 
 fn greet_player(query: Query<(&Name, &Position, &Health, &Ammo, &Player)>) {
